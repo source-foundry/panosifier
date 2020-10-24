d3d2436eecbb5ee37992e63f6220517891309efa
Chris Simpkins <git.simpkins@gmail.com>
2020-10-24 17:30:59 -0400

    [__main__] add CL arg definitions

--- lib/panosifier/__main__.py
+++ lib/panosifier/__main__.py
@@ -17,8 +17,24 @@ def run(argv: List[str]) -> None:
     # ===========================================================
     parser = argparse.ArgumentParser(description="Font panose data editor")
     parser.add_argument(
-        "--version", action="version", version="panosifier v{}".format(__version__)
+        "-v", "--version", action="version", version=f"panosifier v{__version__}"
     )
+    parser.add_argument(
+        "--panose", type=str, required=False, help="comma delimited panose value list"
+    )
+    parser.add_argument("--familytype", type=int, required=False, help="FamilyType value")
+    parser.add_argument("--serifstyle", type=int, required=False, help="SerifStyle value")
+    parser.add_argument("--weight", type=int, required=False, help="Weight value")
+    parser.add_argument("--proportion", type=int, required=False, help="Propotion value")
+    parser.add_argument("--contrast", type=int, required=False, help="Contrast value")
+    parser.add_argument(
+        "--strokevar", type=int, required=False, help="StrokeVariation value"
+    )
+    parser.add_argument("--armstyle", type=int, required=False, help="ArmStyle value")
+    parser.add_argument("--letterform", type=int, required=False, help="Letterform value")
+    parser.add_argument("--midline", type=int, required=False, help="Midline value")
+    parser.add_argument("--xheight", type=int, required=False, help="XHeight value")
+    parser.add_argument("PATH", nargs="+", help="Font file path")
     args = parser.parse_args(argv)
 
     if args.version:
